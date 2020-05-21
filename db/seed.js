593299f5fea8ae7d9ebb593f0a0b4aa3bc62b72b
cficht <chris.ficht@gmail.com>
2020-05-21 11:55:35 -0700

    poll seed

--- db/seed.js
+++ db/seed.js
@@ -3,9 +3,11 @@ const User = require('../lib/models/User/User');
 const Restaurant = require('../lib/models/Restaurant/Restaurant');
 const Offering = require('../lib/models/Offering/Offering');
 const Order = require('../lib/models/Order/Order');
+const Poll = require('../lib/models/Poll/Poll');
 const user_seed = require('./seed-users.json');
 const restaurant_seed = require('./seed-restaurants.json');
 const offering_seed = require('./seed-offerings.json');
+const poll_seed = require('./seed-polls.json');
 
 module.exports = async() => {
   await User.create({
@@ -61,5 +63,20 @@ module.exports = async() => {
     quantity: chance.integer({ min: 0, max: 50 }),
     pickUpDate: chance.date(),
     orderStatus: chance.pickone(orderStatus)
+  })));
+
+  await Poll.create(poll_seed.map(poll => ({
+    name: poll.name,
+    end: poll.end,
+    status: poll.status,
+    restaurant: chance.pickone(restaurants)._id,
+    offering1Name: poll.offering1Name,
+    offering1Votes: poll.offering1Votes,
+    offering1ImageUrl: poll.offering1ImageUrl,
+    offering1Description: poll.offering1Description,
+    offering2Name: poll.offering2Name,
+    offering2Votes: poll.offering2Votes,
+    offering2ImageUrl: poll.offering2ImageUrl,
+    offering2Description: poll.offering2Description
   })));
 };
