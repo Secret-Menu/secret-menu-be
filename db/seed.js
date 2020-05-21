85c9c838d7c02d7aab2ffc0bb50ec73f344e05b9
cficht <chris.ficht@gmail.com>
2020-05-21 11:22:13 -0700

    restaurant seed data

--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,7 @@
 const User = require('../lib/models/User/User');
+const Restaurant = require('../lib/models/Restaurant/Restaurant');
 const users = require('./seed-users.json');
+const restaurants = require('./seed-restaurants.json');
 
 module.exports = async() => {
   await User.create({
@@ -16,5 +18,22 @@ module.exports = async() => {
     email: user.email,
     role: user.role,
     password: user.password
+  })));
+
+  await Restaurant.create(restaurants.map(restaurant => ({
+    restaurantName: restaurant.restaurantName,
+    phoneNumber: restaurant.phoneNumber,
+    category: restaurant.category,
+    quadrant: restaurant.quadrant,
+    address: {
+      streetAddress: restaurant.address.streetAddress,
+      city: restaurant.address.city,
+      state: restaurant.address.state, 
+      zipcode: restaurant.address.zipcode
+    },
+    description: restaurant.description,
+    imageUrl: restaurant.imageUrl,
+    websiteUrl: restaurant.websiteUrl,
+    email: restaurant.email
   })));
 };
